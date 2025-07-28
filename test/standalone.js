8fef97bcccbcdf767cecedf786927d1808385d64
axel669 <haryuu.hanekata@gmail.com>
2025-07-28 09:12:34 -0700

    [update] starting to write out better tests

--- test/standalone.js
+++ test/standalone.js
@@ -1,11 +1,22 @@
-import http from "../node.js"
+// @ts-check
+import http from "@axel669/http"
 
-const testArgs = {
-    url: "https://echo.axel669.net/delay/2",
-    query: {
-        a: 1,
-        b: 2,
-    },
-    data: [1, 2, 3, 4],
-    signal: controller.signal
-}
+const echo = http.origin("https://echo.axel669.net")
+
+const thing = await echo.post`/`({
+    // body: new URLSearchParams({ test: "10" }),
+    json: [1, 2, 3, 4],
+    headers: {
+        "content-type": "application/json"
+    }
+}).promise
+
+console.log(thing)
+console.log(await thing.res.json())
+
+const get = await echo.get`/some/path/example-text`().promise
+console.log(await get.res.json())
+
+const standalone = http.req.get`https://echo.axel669.net/delay/2`()
+setTimeout(() => standalone.abort(), 500)
+console.log(await standalone.promise)
